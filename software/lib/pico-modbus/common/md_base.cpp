080063981b3837d7306458bb5b20c55d90f608b0
SouprP <67385974+SouprP@users.noreply.github.com>
2025-11-07 13:00:01 +0100

    Windoows

--- software/lib/pico-modbus/common/md_base.cpp
+++ software/lib/pico-modbus/common/md_base.cpp
@@ -1,5 +1,9 @@
 #include "md_base.h"
 
+// #include "hardware/i"
+// #include "pico/stdlib.h"
+#include "pico/rand.h"
+
 void ModbusBase::on_debug(const std::function<void(const uint8_t&, const modbus_frame_t&)>& callback) {
     debug_callback = callback;
 }
@@ -11,4 +15,13 @@ void ModbusBase::on_error(const std::function<void(const uint8_t&, const modbus_
 void ModbusBase::on_message(const std::function<void(const uint8_t&, const modbus_frame_t&)>& callback) {
     message_callback = callback;
 }
+
+void ModbusBase::read(modbus_frame_t *frame, bool timeout) {
+    // auto time = get_absolute_time();
+
+    // get_rand_32() % 100;
+    uint8_t data = get_rand_32() % 100;
+    message_callback(data, *frame);
+}
+
 
--- software/src/main.cpp
+++ software/src/main.cpp
@@ -2,12 +2,16 @@
 #include <pico/stdio.h>
 #include "pico/stdlib.h"
 
+#include "pico/rand.h"
+#include "pico/unique_id.h"
 
 #include "pico-modbus/master/master.h"
 #include "pico-modbus/common/md_base.h"
 
 #include "pico/cyw43_arch.h"
 #include "boards/pico2_w.h"
+
+// #include "hardware/i2c.h"
 
 void setup() {
     stdio_init_all();
@@ -23,19 +27,25 @@ int main() {
     stdio_init_all();
     // setup();
     ModbusBase modbus;
-    // modbus.on_message([](const uint8_t id, const modbus_frame_t frame) {
-    //
-    // });
+    modbus.on_message([](const uint8_t id, const modbus_frame_t frame) {
+        printf("id: %u\n", id);
+    });
     if (cyw43_arch_init()) {
         return -1;
     }
 
+    pico_unique_board_id_t id;
+    pico_get_unique_board_id(&id);
+
+    // get_rand_32() ;
+
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(500);
+        sleep_ms(2500);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(500);
-        printf("LED\n");
+        sleep_ms(2500);
+        modbus.read(nullptr, 1);
+        // printf("LED\n");
     }
 
     return 0;
